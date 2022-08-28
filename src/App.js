c51f4ad50153e3cc5bd646981365f25c4f39e3e3
Mike <dev.mhowells@gmail.com>
2022-08-28 19:44:21 +0100

    built out back of article cards, added toggle for displaying front and back

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ import emptyDot from "./icons/empty-dot.png";
 
 function App() {
   const [featuredDetails, setFeaturedDetails] = React.useState(false);
+  const [cardDetails, setCardDetails] = React.useState(false);
 
   function toggleFeaturedDetails() {
     setFeaturedDetails(!featuredDetails);
+  }
+
+  function toggleCardDetails() {
+    setCardDetails(!cardDetails);
   }
 
   return (
@@ -45,9 +50,9 @@ function App() {
                   this long but probably a bit longer, I don't know.
                 </p>
                 <h4>Level: intermediate</h4>
-                <p>
+                {/* <p>
                   <b>Author:</b> Brian Smelter
-                </p>
+                </p> */}
                 <div className="featured-buttons">
                   <button onClick={toggleFeaturedDetails}>details</button>
                   <button>read</button>
@@ -108,24 +113,47 @@ function App() {
           </div>
         </div>
         <div className="articles-display">
-          <div className="article-card">
-            <div className="circle-image-border">
-              <img className="featured-image"></img>
+          {!cardDetails ? (
+            <div className="article-card">
+              <div className="circle-image-border">
+                <img className="featured-image"></img>
+              </div>
+              <h3>The Title of the Card</h3>
+              <p>
+                Just some text explaining stuff about the card. Maybe it's this
+                long.
+              </p>
+              <h4>Level: Intermediate</h4>
+              <div className="card-buttons">
+                <button onClick={toggleCardDetails}>details</button>
+                <button>read</button>
+              </div>
             </div>
-            <h3>The Title of the Card</h3>
-            <p>
-              Just some text explaining stuff about the card. Maybe it's this
-              long.
-            </p>
-            <h4>Level: Intermediate</h4>
-            {/* <p>
-              <b>Author:</b> Brian Smelter
-            </p> */}
-            <div className="card-buttons">
-              <button>details</button>
-              <button>read</button>
+          ) : (
+            <div className="article-card-back">
+              <h3 id="featured-back-title">The title of the card</h3>
+              <div className="detail-block">
+                <h3 className="subheading-back">Paragraphs:</h3>
+                <p>3</p>
+              </div>
+              <div className="detail-block">
+                <h3 className="subheading-back">Theme:</h3>
+                <p>people</p>
+              </div>
+              <div className="detail-block">
+                <h3 className="subheading-back">Vocabulary:</h3>
+                <ul>
+                  <li>banana</li>
+                  <li>apple</li>
+                  <li>monkey</li>
+                </ul>
+              </div>
+              <div className="featured-back-buttons">
+                <button onClick={toggleCardDetails}>back</button>
+                <button>read</button>
+              </div>
             </div>
-          </div>
+          )}
           <div className="article-card"></div>
           <div className="article-card"></div>
         </div>
